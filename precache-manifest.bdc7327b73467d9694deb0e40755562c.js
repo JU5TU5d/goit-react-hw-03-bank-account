e51a42002430b9d9c08e2923c6b51f8f2b8621e5
JU5TU5d <vladislav261100@gmail.com>
2019-10-23 08:40:27 +0300

    Updates

---
+++ precache-manifest.bdc7327b73467d9694deb0e40755562c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9798bdec642a57f8f70ca21b35f9054",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "225f0cc2fb7db45d0fd1",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "60afde3891b7a764a418",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.bf778552.chunk.css"
+  },
+  {
+    "revision": "225f0cc2fb7db45d0fd1",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.68666373.chunk.js"
+  },
+  {
+    "revision": "60afde3891b7a764a418",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.1093a419.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
